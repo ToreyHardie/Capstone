b4132c6a444565b48ac943902832264ec96a1391
Torey Hardie <Torey.walker@gmail.com>
2024-01-29 01:18:09 -0600

    added some changes

--- Capstone/src/components/cart.jsx
+++ Capstone/src/components/cart.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 
 export default function Cart({ cart, removeFromCart, clearCart }) {
-  // State to track cart items and their quantities
   const [cartItems, setCartItems] = useState(cart);
 
-  // Function to handle quantity change
   const handleQuantityChange = (index, newQuantity) => {
     if (!isNaN(newQuantity) && newQuantity >= 1) {
       const updatedCartItems = [...cartItems];
@@ -13,22 +11,19 @@ export default function Cart({ cart, removeFromCart, clearCart }) {
     }
   };
 
-  // Function to calculate total price for a single item
   const calculateItemTotal = (price, quantity) => {
     return price * quantity;
   };
 
-  // Function to calculate total price for all items in the cart
   const calculateTotal = () => {
     let total = 0;
     cartItems.forEach(item => {
-      const quantity = item.quantity || 1; // Initialize to 1 if quantity is undefined or falsy
+      const quantity = item.quantity || 1;
       total += calculateItemTotal(item.price, quantity);
     });
     return total;
   };
 
-  // Function to remove an item from the cart
   const handleRemoveFromCart = (index) => {
     const updatedCartItems = [...cartItems];
     updatedCartItems.splice(index, 1);
@@ -36,7 +31,6 @@ export default function Cart({ cart, removeFromCart, clearCart }) {
     removeFromCart(index);
   };
 
-  // Function to clear the entire cart
   const handleClearCart = () => {
     setCartItems([]);
     clearCart();
@@ -68,12 +62,12 @@ export default function Cart({ cart, removeFromCart, clearCart }) {
               <td>
                 <input
                   type="number"
-                  value={item.quantity || 1} // Default to 1 if item.quantity is undefined or falsy
+                  value={item.quantity || 1}
                   onChange={(e) => handleQuantityChange(index, parseInt(e.target.value))}
                   min="1"
                 />
               </td>
-              <td>${calculateItemTotal(item.price, item.quantity || 1)}</td> {/* Calculate total with default quantity */}
+              <td>${calculateItemTotal(item.price, item.quantity || 1)}</td>
               <td>
                 <button onClick={() => handleRemoveFromCart(index)}>Remove</button>
               </td>
@@ -86,4 +80,3 @@ export default function Cart({ cart, removeFromCart, clearCart }) {
     </div>
   );
 }
-
